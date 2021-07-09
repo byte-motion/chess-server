64561c9936c450a7eeafd9e2c11a70fd4f7ee39c
DomagojKudek <qudek77@gmail.com>
2021-07-09 11:06:49 +0200

    Switched to move time instead of depth
    Move time is now the second input variable instead of depth.
    
    Test for the score parameter.

--- app/chess-server.js
+++ app/chess-server.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var compression = require('compression');
-//var apicache = require('apicache').options({ debug: true }).middleware;
-//var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
 var urlencode = require('urlencode');
-//app.use(bodyParser.urlencoded({extended: true}));
 
 var stockfish = require("stockfish");
 
@@ -16,50 +13,55 @@ function getBestMove(options, callback) {
   var instance = stockfish();
 
   // set the callback for each message
-  instance.onmessage = function(event) {
+  instance.onmessage = function (event) {
     var message = event.data || event;
-    
+
     console.log(typeof event, event);
-    
+
     if (typeof message === 'string') {
       //'bestmove g1f3 ponder g8f6'
       var split = message.split(/ +/);
+      var scoreStats = 0;
+      for (var i = 0; i < split.length; i++) {
+        if(split[i]=="scorecp"){
+          scoreStats = split[i+1];
+        }
+      }
       if (split[0] == 'bestmove') {
-        callback(split[1]);
+        callback(new Array(split[1], split, scoreStats));
       }
     }
   };
 
-  // set the fen, then perform the search with the given depth
+  // set the fen, then perform the search with the given time
   instance.postMessage('position fen ' + options.fen);
-  instance.postMessage('go depth ' + options.depth);
+  instance.postMessage('go movetime ' + options.time);
 }
 
-//app.use(compression());
 app.use(compression({
-  threshold : 0, // or whatever you want the lower threshold to be
-  filter    : function(req, res) {
+  threshold: 0,
+  filter: function (req, res) {
     return true;
   }
 }));
 
 console.log('registering /');
-app.get('/' /*, apicache('5 minutes')*/, function(req, res) {
+app.get('/', function (req, res) {
   console.log('GET ' + req.originalUrl);
-  //res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
+
 
   res.set({
     'Content-Type': 'application/json'
   });
 
   res.status(200);
-  res.send({"apis": ['/moves']});
+  res.send({ "apis": ['/moves'] });
 });
 
 console.log('registering /moves');
-app.get('/moves' /*, apicache('5 minutes')*/, function(req, res) {
+app.get('/moves', function (req, res) {
   console.log('GET ' + req.originalUrl);
-  //res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
+
 
   res.set({
     'Content-Type': 'application/json'
@@ -70,30 +72,27 @@ app.get('/moves' /*, apicache('5 minutes')*/, function(req, res) {
     res.status(500);
     res.send({
       "error": "Parameter 'fen' is required.",
-      "suggestion": '/moves?fen=' + urlencode('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1') + '&depth=15'
+      "suggestion": '/moves?fen=' + urlencode('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1') + '&movetime=15'
     });
     return;
   }
 
-  var depth = parseInt(req.query.depth, 10) || 15;
-  depth = Math.min(depth, 20);
+  var time = parseInt(req.query.time, 10) || 15;
+  time = Math.min(time, 20);
 
-  getBestMove({fen: fen, depth: depth}, function(moveResponse, err) {
+  getBestMove({ fen: fen, time: time }, function (moveResponse, err) {
     if (err) {
       console.error(err);
       res.status(500);
-      res.send({"error": err});
+      res.send({ "error": err });
       return;
     }
 
     res.status(200);
-    res.send({"bestmove": moveResponse, "depth": depth});
+    res.send({ "bestmove": moveResponse[0], "cjeli string" : moveResponse[1], "scoreCP" : moveResponse[2], "time": time });
   });
-
-  //res.send(JSON.stringify({apis: apis}, 0, 4));
 });
 
 var port = settings.port;
 app.listen(port);
 console.log('listening on ' + port);
-
